15db80b9d80d9870db291027abd02150a17416b7
leticiacarvalho04 <leticiahelena.oliveira@outlook.com>
2025-09-12 21:00:00 -0300

    🐛 fix: Corrigindo rota do backend

--- src/services/axiosService.tsx
+++ src/services/axiosService.tsx
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "https://seu-backend.com/api", // ajuste para sua URL real
+  baseURL: "https://localhost:5000",
   headers: {
     "Content-Type": "application/json",
   },
