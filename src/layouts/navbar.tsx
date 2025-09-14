e7b57a9cff0f7b6cfaeb3178cb68f71c0e00cca6
Victor Salles <victorgamesbr091@gmail.com>
2025-09-14 15:16:15 -0300

    🐛fix: Links ativos da navbar

--- src/layouts/navbar.tsx
+++ src/layouts/navbar.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import "../styles/navbar.css";
 import logo from "../assets/logoDomRock.png";
 import { Button } from "../components/button";
@@ -7,22 +7,22 @@ import { Button } from "../components/button";
 export function Navbar() {
   return (
     <nav className="container">
-      <Link to="/">
+      <NavLink to="/">
         <img src={logo} alt="Logo" className="logo" />
-      </Link>
+      </NavLink>
       <div className="rotas">
-        <Link to="/home" className="nav-link">
+        <NavLink to="/home" className="nav-link">
           Início
-        </Link>
-        <Link to="/chat" className="nav-link">
+        </NavLink>
+        <NavLink to="/chat" className="nav-link">
           Chat
-        </Link>
-        <Link to="/usuarios" className="nav-link">
+        </NavLink>
+        <NavLink to="/usuarios" className="nav-link">
           Usuários
-        </Link>
-        <Link to="/relatorios" className="nav-link">
+        </NavLink>
+        <NavLink to="/relatorios" className="nav-link">
           Relatórios
-        </Link>
+        </NavLink>
         <Button label={"Sair"}  />
       </div>
     </nav>
